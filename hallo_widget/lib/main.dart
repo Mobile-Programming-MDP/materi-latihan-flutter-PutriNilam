99d5f54cdd29ed04ef5cce32e7dc72f96c4b13a2
Putri Nilam <putrinilam_2226250116@mhs.mdp.ac.id>
2023-10-13 18:43:54 +0700

    Second commit

--- hallo_widget/lib/main.dart
+++ hallo_widget/lib/main.dart
@@ -3,22 +3,31 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const Mywidget());
 }
+
 class Mywidget extends StatelessWidget {
   const Mywidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    title: "Hallo Widget",
-    theme : ThemeData(primarySwatch:Colors.blue),
-    home : Scaffold(
-    appBar : AppBar(title: const Text("Putri Nilam")),
-    body : const Center (
-    child: Text ("Hallo Nilam"),
-    ),));
+        title: "Hallo Widget",
+        theme: ThemeData(primarySwatch: Colors.red),
+        home: FirstScreen());
   }
+}
 
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
 
-
-  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Nilam"),
+      ),
+      body: const Center(
+        child: Text("Hallo Nilam"),
+      ),
+    );
+  }
 }
