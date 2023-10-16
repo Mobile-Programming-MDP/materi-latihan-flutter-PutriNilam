1465f117057126c10cb5cbc41c19abf096317786
PutriNilam <putrinilam_2226250116@mhs.mdp.ac.id>
2023-10-16 18:17:02 +0700

    First commit

--- hallo_widget/lib/main.dart
+++ hallo_widget/lib/main.dart
@@ -1,33 +1,74 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Mywidget());
+  runApp(const MyApp());
 }
 
-class Mywidget extends StatelessWidget {
-  const Mywidget({super.key});
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Hallo Widget",
-        theme: ThemeData(primarySwatch: Colors.red),
-        home: FirstScreen());
+        title: "Putri Nilam",
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: firstScreen());
   }
 }
 
-class FirstScreen extends StatelessWidget {
-  const FirstScreen({super.key});
+class firstScreen extends StatelessWidget {
+  const firstScreen({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Nilam"),
-      ),
-      body: const Center(
-        child: Text("Hallo Nilam"),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Widget Demo'),
+        ),
+        body: Column(
+          children: [
+            Container(
+              height: 200,
+              width: double.infinity,
+              margin: const EdgeInsets.all(16),
+              padding: const EdgeInsets.all(16),
+              color: Colors.blue,
+              child: Text(
+                'Ini adalah contoh penggunaan Container',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+                child: const Text('Elevated Button')),
+            const SizedBox(
+              height: 10,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.star, color: Colors.amber),
+                Text(
+                  'Rating 4,5',
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.network(
+                'https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg',
+                width: double.infinity,
+                height: 200,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ));
   }
 }
