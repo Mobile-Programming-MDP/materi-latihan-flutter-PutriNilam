10fc7b80327afbd1aa4bd6593700ca3bab109aa2
Putri Nilam <putrinilam_2226250116@mhs.mdp.ac.id>
2023-10-23 18:59:10 +0700

    updt

--- hallo_widget/lib/main.dart
+++ hallo_widget/lib/main.dart
@@ -6,69 +6,103 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Putri Nilam",
-        theme: ThemeData(primarySwatch: Colors.blue),
-        home: firstScreen());
+      title: "Hello Widget",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      //home: FirstScreen()
+      home: const WidgetDemo(),
+    );
   }
 }
 
-class firstScreen extends StatelessWidget {
-  const firstScreen({super.key});
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Widget Demo'),
-        ),
-        body: Column(
-          children: [
-            Container(
-              height: 200,
-              width: double.infinity,
-              margin: const EdgeInsets.all(16),
-              padding: const EdgeInsets.all(16),
-              color: Colors.blue,
+      appBar: AppBar(
+        title: const Text('Nur Rachmat'),
+        actions: <Widget>[
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                color: Colors.white,
+              )),
+        ],
+        leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.home,
+              color: Colors.white,
+            )),
+      ), //Appbar
+
+      body: const Center(
+        child: Text("Hello Nur Rachmat"),
+      ),
+    );
+  }
+}
+
+class WidgetDemo extends StatelessWidget {
+  const WidgetDemo({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Widget Demo'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 200,
+            margin: const EdgeInsets.all(16),
+            padding: const EdgeInsets.all(16),
+            color: Colors.blue,
+            child: const Center(
               child: Text(
                 'Ini adalah contoh penggunaan Container',
                 style: TextStyle(
                   fontSize: 20.0,
                   color: Colors.white,
                 ),
+                textAlign: TextAlign.center,
               ),
             ),
-            ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                ),
-                child: const Text('Elevated Button')),
-            const SizedBox(
-              height: 10,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text('Tombol Elevated'),
             ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.star, color: Colors.amber),
-                Text(
-                  'Rating 4,5',
-                )
-              ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.star, color: Colors.amber),
+              Text('Rating: 4.5'),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.network(
+              'https://picsum.photos/id/7/300/200',
+              width: double.infinity,
+              height: 200,
+              fit: BoxFit.cover,
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.network(
-                'https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg',
-                width: double.infinity,
-                height: 200,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
